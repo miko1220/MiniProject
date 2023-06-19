1938b568c23a1c39f984fdadac2a8c5fe5a2a7a9
miko1220 <mamiko.k1220@gmail.com>
2023-06-19 15:16:07 +0900

    Initial Commit

---
+++ src/main/java/kr/demo/first/controller/MainController.java
@@ -0,0 +1,17 @@
+package kr.demo.first.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {
+	
+	@GetMapping("/")
+	public String home() {
+		return "home";
+	}
+	@GetMapping("/index")
+	public String index() {
+		return "index";
+	}
+}
---
+++ src/test/java/kr/demo/first/SpringFirstProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.demo.first;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringFirstProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
