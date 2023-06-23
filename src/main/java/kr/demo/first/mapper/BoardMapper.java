e1f2d8d1fa73b9a452b0a06f0386c4204109b713
miko1220 <mamiko.k1220@gmail.com>
2023-06-23 14:34:09 +0900

    세부 수정

--- src/main/java/kr/demo/first/mapper/BoardMapper.java
+++ src/main/java/kr/demo/first/mapper/BoardMapper.java
@@ -9,8 +9,16 @@ import kr.demo.first.vo.BoardVO;
 
 @Mapper
 public interface BoardMapper {
+	// 글 추가하기
 	void insert(Map<String, String> insertMap) throws Exception;
+	
+	// 글 수정하기
 	void update(Map<String, String> updateMap) throws Exception;
+	
+	// 글 상세 조회하기
 	BoardVO detail (int idx) throws Exception;
+	
+	// 글 삭제
+	void delete(int idx) throws Exception;
 }
 
