8f5e5e6efab6139e4ec4afa59d2d2c5bbf330f86
miko1220 <mamiko.k1220@gmail.com>
2023-07-26 14:10:42 +0900

    insert UI변경

--- src/main/java/kr/demo/first/controller/MainController.java
+++ src/main/java/kr/demo/first/controller/MainController.java
@@ -10,7 +10,7 @@ public class MainController {
 	public String index() {
 		return "index";
 	}
-	@GetMapping("/signin")
+	@GetMapping({"/signin","/"})
 	public String signin() {
 		return "signin";
 	}
